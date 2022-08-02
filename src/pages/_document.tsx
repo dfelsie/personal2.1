775a36c58c513ebaa5b5b3577f7c21fa8b3a33a8
dfelsie <dfelsenthal88@gmail.com>
2022-08-01 23:16:46 -0400

    Try removing head from doc

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,22 +7,7 @@ import "@fontsource/montserrat/700.css"; */
 export default function Document() {
   return (
     <Html>
-      <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin="true"
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Inter&display=optional"
-          rel="stylesheet"
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Ubuntu&display=optional"
-          rel="stylesheet"
-        />
-      </Head>
+      <Head></Head>
       <body>
         <Main />
         <NextScript />
